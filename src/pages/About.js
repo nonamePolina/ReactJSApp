9ada93470a3d06908e29e0b3aee536f398cf6661
gavricheva <fasolkad@mail.ru>
2021-09-26 18:51:17 +0300

    Create App

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>
+                About page
+            </h1>
+        </Fragment>
+    )
+}
